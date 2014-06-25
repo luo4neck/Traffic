68c6948822397bc0799816decc63c522a3f60daf
QIYUAN LUO <luoq@chuck.tchpc.tcd.ie>
2014-06-25 01:23:17 +0100

    alltogether is fine

--- main.cc
+++ main.cc
@@ -23,19 +23,14 @@ int main(int argc, char *argv[])
 	{
 		// broadcast and recv the ends..
 		int *car_back = new int[nps];
-		int *car_send = new int[nps];
-		
 		int my_back = car.back();
-		printf("%d, %d\n", myid, my_back);
+		//printf("%d, %d\n", myid, my_back);
 
-		//car_back[myid] = car.back(); 
-		car_send[myid] = car.back(); 
 		
 		//Here should using allgather because each car have to send its location to all other cars, then recv location of all other cars..
-		//MPI_Allgather(&my_back, sizeof(int), MPI_INT, car_back, sizeof(int)*nps, MPI_INT, comm);
-		MPI_Allgather(car_send, 1, MPI_INT, car_back, nps, MPI_INT, comm);
+		MPI_Allgather(&my_back, 1, MPI_INT, car_back, 1, MPI_INT, comm);
 		
-		printf("i am %d, i have %d, %d, %d \n", myid, car_back[0], car_back[1], car_back[2]);
+		//printf("i am %d, i have %d, %d, %d \n", myid, car_back[0], car_back[1], car_back[2]);
 		
 		
 		// find the smallest distance from previous car..
@@ -49,6 +44,8 @@ int main(int argc, char *argv[])
 				smallest[1] = dis; 
 			}
 		}
+		printf("i am %d, previous is %d, distance %d \n", myid, smallest[0], smallest[1]);
+
 		//delete car_back;
 		
 		// go or stop
