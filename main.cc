84878a5a010f4e1a7e5fbb30bcdc2ad38ca516ee
luo4neck <luo4neck@gmail.com>
2014-08-31 16:17:35 +0100

    ready to do performance test

--- main.cc
+++ main.cc
@@ -15,15 +15,28 @@ int main(int argc, char *argv[])
 	
 	// session 4
 	cout<<"Map constructing..."<<endl;
-	int ewnum = 4;
-	int nsnum = 4;
-	int *ewrange = new int[ewnum]; 
-	int *nsrange = new int[nsnum];
-	for(int i=0; i<ewnum; ++i) {	ewrange[i] = 100 * i + 50;	}
-	for(int i=0; i<nsnum; ++i) {	nsrange[i] = 100 * i + 50;	}
 	
-	BOUND bound(400, 0, 400, 0, ewnum, nsnum, ewrange, nsrange);
+	// large map test..
+		int ewnum = 16;
+		int nsnum = 16;
+		int *ewrange = new int[ewnum]; 
+		int *nsrange = new int[nsnum];
+		for(int i=0; i<ewnum; ++i) {	ewrange[i] = 100 * i + 50;	}
+		for(int i=0; i<nsnum; ++i) {	nsrange[i] = 100 * i + 50;	}
+		BOUND bound(1600, 0, 1600, 0, ewnum, nsnum, ewrange, nsrange);
 			  //e,    w, n,    s, 
+	
+/*	{// gif generate..
+		int ewnum = 4;
+		int nsnum = 4;
+		int *ewrange = new int[ewnum]; 
+		int *nsrange = new int[nsnum];
+		for(int i=0; i<ewnum; ++i) {	ewrange[i] = 100 * i + 50;	}
+		for(int i=0; i<nsnum; ++i) {	nsrange[i] = 100 * i + 50;	}
+		BOUND bound(400, 0, 400, 0, ewnum, nsnum, ewrange, nsrange);
+			  //e,    w, n,    s, 
+	} */
+
 	int ewns[4] = {-1, -1, -1, -1};
 	bound.Construct(spot, cross, ewns); // construct the map in this process..
 	// session 4	
@@ -32,20 +45,18 @@ int main(int argc, char *argv[])
 
 	cout<<"Cars constructing..."<<endl;
 	srand48(time(NULL));
-
 	int car_num = atoi(argv[1]);
-	cout<<car_num<<endl;
 	Add_Car(bound, car, ewnum, nsnum, car_num, spot);
 	
 	cout<<car.size()<<" cars ("<<car.size()*sizeof(car)/1024<<"KB) constructed on the map"<<endl<<endl;
 
-	int time_i = 0, time_max = 300;
-	long int accu = 0;
+	int time_i = 0, time_max = 600;
+	//long int accu = 0;
 	time_t start = time(NULL);
 	cout<<"Simulation Start!"<<endl;
 	while(time_i < time_max ) // main loop.. one loop is one time step..
 	{
-		ofstream file("plot_cars.dat");// plot
+//		ofstream file("plot_cars.dat");// plot
 		
 		if ( time_i%20 == 0 ) Signal_Switch(cross);
 		cout<<"At time "<<time_i<<" "<<endl;
@@ -59,10 +70,10 @@ int main(int argc, char *argv[])
 			
 			caritr->space_detect(rand, newx, newy, newdrct, spot, cross, turn);
 			
-			accu = accu + abs( caritr->X() - newx ) + abs( caritr->Y() - newy );
+//			accu = accu + abs( caritr->X() - newx ) + abs( caritr->Y() - newy );
 			
 			caritr->Move(newx, newy, newdrct, spot);
-			file<<caritr->X()<<" "<<caritr->Y()<<endl;   // plot
+//			file<<caritr->X()<<" "<<caritr->Y()<<endl;   // plot
 		}
 		
 		{ // this is scope is for car delete from the map..
@@ -76,8 +87,8 @@ int main(int argc, char *argv[])
 			}
 			car.remove_if( check_del() );
 		}
-		file.close();  // plot
-		
+//		file.close();  // plot
+/*		
 		FILE *gp = popen("gnuplot -persist", "w");
    		if(gp == NULL)
 		{
@@ -93,21 +104,19 @@ int main(int argc, char *argv[])
 		fprintf(gp, "set yrange[%d: %d]\n", bound.St(), bound.Nt() );
 		fprintf(gp, "plot 'plot_road.dat' u 1:2 title 'Road Spot' w points ps 1 pt 3 lt 2, 'plot_cars.dat' u 1:2 title 'Cars' w points ps 1 lt 1\n");
 		fclose(gp);
-		// session 4..
-	
+*/	
 		time_t end = time(NULL);
 		cout<<end-start<<" seconds spent for this step"<<endl<<endl;
 		
 		time_i++;
 
 		if( (int)car.size() < car_num ) Add_Car(bound, car, ewnum, nsnum, car_num - car.size(), spot);
-		cout<<car.size()<<endl;
 	}
 	
-	system("convert -delay 20 -loop 0 *.png Moving.gif\n");	// session 4..
+//	system("convert -delay 20 -loop 0 *.png Moving.gif\n");	
 	
-	double re = (double)accu/ (double)time_max/ (double)car_num ;
-	cout<<"Average movement in this test is: "<<re<<endl;
+//	double re = (double)accu/ (double)time_max/ (double)car_num ;
+//	cout<<"Average movement in this test is: "<<re<<endl;
 	
 	return 0;
 }
